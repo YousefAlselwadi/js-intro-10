603bd4d7229a08bd8f89603f5ca55addec10e4b3
YousefAlselwadi <yalselwadi@gmail.com>
2024-08-25 10:06:51 -0500

    new loop files

---
+++ 09-js-loops/03-while-loop.js
@@ -0,0 +1,44 @@
+const { getRandomNumber } = require('../utils/MathHelper.js');
+
+
+let written = 4;
+let guess = undefined;
+let attempts = 0;
+
+while(written !== guess) {
+    guess = getRandomNumber(1, 10);
+    console.log(guess);
+    attempts++;
+}
+
+console.log(`We got the matching number finally after ${attempts} atttempts!`);
+
+
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let num = 1;
+
+while(num <= 10) {
+    console.log(num);
+    num++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Infinite loop
+while(true) {
+    console.log('Hello');
+}
