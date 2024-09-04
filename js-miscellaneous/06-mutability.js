690d458db03f639e27a0af7ec1e86c64c0ae339b
YousefAlselwadi <yalselwadi@gmail.com>
2024-09-04 18:17:22 -0500

    fixes

--- js-miscellaneous/06-mutability.js
+++ js-miscellaneous/06-mutability.js
@@ -1,5 +1,4 @@
 const arr1 = [ 1, 2, 3 ];
-
 const arr2 = arr1;
 
 console.log(arr1); // [ 1, 2, 3 ]
@@ -11,53 +10,54 @@ console.log(arr1); // [ 1, 2, 3, 4 ]
 console.log(arr2); // [ 1, 2, 3, 4 ]
 
 
-// perimitives
+// pritimives are always immutable
 let str1 = 'John';
 let str2 = str1;
 
-console.log(str1); // 'John'
-console.log(str2); // 'John'
+console.log(str1); // John
+console.log(str2); // John
 
-str1 = 'james'
+str1 = 'James';
 
-console.log(str1);
-console.log(str2);
+console.log(str1); // James 
+console.log(str2); // John
 
 
-// Coppying your array to prevent multability by using spread operator
+
+// Copying your array to prevent mutability by using spread operator
 const numbers1 = [ 1, 2, 3, 4, 5 ];
-const numbers2 = [...numbers1 ];
+const numbers2 = [ ...numbers1 ];
 
-console.log(numbers1);
-console.log(numbers2);
+console.log(numbers1); // [ 1, 2, 3, 4, 5 ]
+console.log(numbers2); // [ 1, 2, 3, 4, 5 ]
 
 numbers1.push(6);
 
-console.log(numbers1);
-console.log(numbers2);
+console.log(numbers1); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(numbers2); // [ 1, 2, 3, 4, 5 ]
 
-// Coppying your array to prevent multability by loops
+// Copying your array to prevent mutability by using loops
 const booleans1 = [ true, false, true ];
 const booleans2 = [];
 
 booleans1.forEach((el) => {
-    booleans2.push(el);
+  booleans2.push(el);
 });
 
-console.log(booleans1);
-console.log(booleans2);
+console.log(booleans1); // [ true, false, true ]
+console.log(booleans2); // [ true, false, true ]
 
-booleans1.push('hello')
+booleans1.push('hello');
 
-console.log(booleans1);
-console.log(booleans2);
+console.log(booleans1); // [ true, false, true, 'hello' ]
+console.log(booleans2); // [ true, false, true ]
 
 
-// Spread operator for loops does not work in case of nested arrays or objects 
+// Spread operator or loops does not work in case of nested arrays or objects 
 const groups1 = [
-    'John',
-    ['Alex', 'Mariia'],
-    ['John', 'Jane']
+  'John',
+  ['Alex', 'Mariia'],
+  ['John', 'Jane']
 ];
 
 const groups2 = [ ...groups1 ];
@@ -68,8 +68,8 @@ console.log(groups2); // [ 'John', [ 'Alex', 'Mariia' ], [ 'John', 'Jane' ] ]
 groups1.push('Mark');
 groups1[1][1] = 'Jessica';
 
-console.log(groups1); // [ 'John', [ 'Alex', 'Jessica' ], [ 'John', 'Jane' ], 'Mark' ]
-console.log(groups2); // [ 'John', [ 'Alex', 'Jessica' ], [ 'John', 'Jane' ] ]
+console.log(groups1); // [ 'John', [ 'Alex', 'Mariia' ], [ 'John', 'Jane' ], 'Mark' ]
+console.log(groups2); // [ 'John', [ 'Alex', 'Mariia' ], [ 'John', 'Jane' ] ]
 
-// We call this copying with loops or spread Operator as SHALLOW COPY
-// DEEP COPY: SAFER WAY TO PREVENT MUTIBILITY AND CAN BE ACHIVIED USING JSON
+// We call this copying with loops or spread operator as SHALLOW COPY
+// DEEP COPY: safer way to prevent mutability and can be achieved using JSON
